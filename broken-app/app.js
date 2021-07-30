3fa57cc9863eeb11a39774247fce7eaaa767a65d
Jerry Hsu <Jerry>
2021-07-30 16:30:51 -0400

    Part 3, initial submission (break for github request limit)

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,41 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+
+const app = express();
+const ExpressError = require("./expressError");
+app.use(express.json());
+
+app.get("/", function(req,res){
+  return res.send("<h1>Hello world!</h1>")
+});
 
 app.post('/', function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
+    const devList = req.body.developers;
+    console.log(`devList: ${devList}`)
+    let results = devList.map( dev => {
+      link = `https://api.github.com/users/${dev}`;
+      console.log(link);
+      return axios.get(link)
+        .then(res => {
+          console.log(`res.data from axios.get: ${res.data}`);
+          return res.data;
+        })
+        .catch( e=>console.error(e) );
     });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-    return res.send(JSON.stringify(out));
+    Promise.all(results)
+      .then( res => console.log(`res from promise ${res}`));
+
+    console.log(`results: ${results}`)
+    const output = results.map( (user) => ({ name: user.name, bio: user.bio }));
+    console.log(`output: ${output}`)
+    return res.json(output);
   } catch {
-    next(err);
+    next();
   }
 });
 
-app.listen(3000);
+app.listen(3000, ()=> {
+  console.log("Starting up on port 3000");
+});
